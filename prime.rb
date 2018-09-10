d07ae43515a35387a2dacbfcbd7f236ba9baf280
Beatrice Mendoza <mendoza@nyu.edu>
2018-09-10 01:55:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   if num <= 1
       return false
     else 
-        if (2...Math.sqrt(n).floor).any? { |i| n % i  == 0}
+        if (2...Math.sqrt(num).floor).any? { |n| num % n  == 0}
             return false
         else
             return true
